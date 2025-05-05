c832d2a747cb8c102cf2d589dd8574eb11aaaef4
Rio Taiga <rtaiga119@gmail.com>
2025-05-05 10:42:08 -0700

    Delete client2 directory

--- client2/connectivity_tester.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script tests connectivity to the server and router.
-
-echo "[+] Pinging Server (192.168.56.10)..."
-ping -c 3 192.168.56.10
-
-echo "[+] Pinging Router (192.168.56.1)..."
-ping -c 3 192.168.56.1
